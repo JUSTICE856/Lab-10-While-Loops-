455dd252662096efc9675247469645d3f99d25be
JUSTIC856 <justice.ndukwu@mail.citytech.cuny.edu>
2021-04-26 16:24:21 -0400

    Completed

---
+++ gimmymynumber.py
@@ -0,0 +1,8 @@
+ string = input("Gimmie a number that is greater than 100...")
+ num = int(string)
+ while num < 100:
+    print(num) + " is less than 100, Try again, I've got all day...")
+    userstring = print(num + " is less than 100, Try again, I've got all day...")
+    usernum = int(string)
+
+print(num + "is greater than 100! Finally great job!")
